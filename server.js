ceee3659530de6602827b978beec8268a738f84a
trinity-senpai <sihletrinity70@gmail.com>
2021-08-18 01:05:29 +0200

    setting up

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 //COPYRIGHT SIPHESIHLE BOMELA | ALL RIGHTS RESERVED
+// await fetch("https://integration-gateway.justsolve.co.za/clickatell/V1/sendSMS", {
+//     "credentials": "omit",
+//     "headers": {
+//         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+//         "Accept": "application/json, text/plain, */*",
+//         "Accept-Language": "en-US,en;q=0.5",
+//         "Content-Type": "application/json",
+//         "Save-Data": "on",
+//     },
+//     "referrer": "http://app.sonke.co.za/",
+//     "body": "{\"content\":\"Sonke: Betway FREE Credit Reminder: Register at https://www.betway.co.za/?btag=P73370-PR22258-CM55263-TS1923988/&SignUpCode=d4u2551 to receive your R30 Betway gaming Voucher. For queries contact sonke@tlcrewards.com Ts&Cs apply.\",\"to\":\"27736076544\"}",
+//     "method": "POST",
+//     "mode": "cors"
+// });
+
+// ! STRUCTURE OF SMS
+// {"content":"BetVouch: Betway FREE Credit Reminder: Register at https://www.betway.co.za/?btag=P73370-PR22258-CM55263-TS1923988/&SignUpCode=d4u2551 to receive your R30 Betway gaming Voucher. For queries contact sonke@tlcrewards.com Ts&Cs apply.","to":"27736076544"}
+ 
