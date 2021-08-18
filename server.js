73e5dfc161cc2d86b99f8e8d5742ed239c73452c
trinity-senpai <sihletrinity70@gmail.com>
2021-08-18 14:39:41 +0200

    setup ejs & express parse

--- server.js
+++ server.js
@@ -17,3 +17,12 @@
 // ! STRUCTURE OF SMS
 // {"content":"BetVouch: Betway FREE Credit Reminder: Register at https://www.betway.co.za/?btag=P73370-PR22258-CM55263-TS1923988/&SignUpCode=d4u2551 to receive your R30 Betway gaming Voucher. For queries contact sonke@tlcrewards.com Ts&Cs apply.","to":"27736076544"}
  
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
